c86bce14f9cb68466b2720d45ff1eda3b903bbbc
Muramatsu Yuichi <21cm0136@jec.ac.jp>
2022-12-05 14:51:01 +0900

    commit .gitignore

---
+++ TurnOver/TurnOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnOverApp.swift
+//  TurnOver
+//
+//  Created by cmStudent on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct TurnOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TurnOverUITests/TurnOverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TurnOverUITestsLaunchTests.swift
+//  TurnOverUITests
+//
+//  Created by cmStudent on 2022/12/05.
+//
+
+import XCTest
+
+class TurnOverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
